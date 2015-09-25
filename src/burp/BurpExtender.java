de8dd8125ad9eee6c8b305c7a95b280f4d39bd4e
András Veres-Szentkirályi <vsza@vsza.hu>
2015-09-25 12:51:42 +0200

    basic Python code generation

--- src/burp/BurpExtender.java
+++ src/burp/BurpExtender.java
@@ -33,6 +33,42 @@ public class BurpExtender implements IBurpExtender, IContextMenuFactory
 	}
 
 	private void copyMessages(IHttpRequestResponse[] messages) {
-		// TODO
+		StringBuilder py = new StringBuilder("import requests\n");
+
+		for (IHttpRequestResponse message : messages) {
+			IRequestInfo ri = helpers.analyzeRequest(message);
+			byte[] req = message.getRequest();
+			py.append("\nrequests.");
+			py.append(ri.getMethod().toLowerCase());
+			py.append("(\"");
+			py.append(ri.getUrl().toString());
+			py.append("\", headers={");
+			boolean firstHeader = true;
+			for (String header : ri.getHeaders()) {
+				if (header.startsWith("Host:")) continue;
+				int colonPos = header.indexOf(':');
+				if (colonPos == -1) continue;
+				if (firstHeader) {
+					firstHeader = false;
+					py.append('"');
+				} else {
+					py.append(", \"");
+				}
+				py.append(header, 0, colonPos);
+				py.append("\": \"");
+				py.append(header, colonPos + 2, header.length());
+				py.append('"');
+			}
+			py.append('}');
+			int bo = ri.getBodyOffset();
+			if (bo < req.length - 1) {
+				py.append(", data=\"");
+				py.append(new String(req, bo, req.length - bo));
+				py.append('"');
+			}
+			py.append(')');
+		}
+
+		System.err.println(py.toString()); // TODO clipboard
 	}
 }
